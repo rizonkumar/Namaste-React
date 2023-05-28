8e5c103b716583d7030843b4300aa45e469d5622
rizonkumar <rizon.kumar.rahi.com>
2023-05-28 22:16:34 +0530

    Followed Coding Standard

--- 06-Exploring the World/src/components/Body.js
+++ 06-Exploring the World/src/components/Body.js
@@ -20,11 +20,9 @@ const Body = () => {
     setListOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
   };
 
-  if (listOfRestaurants.length === 0) {
-    return <Shimmer />;
-  }
-
-  return (
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
