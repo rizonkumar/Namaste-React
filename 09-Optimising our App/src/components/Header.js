cd277534b284743be63f3e8866e59a33b27687c9
Rizon Kumar <rizon.kumar.rahi@gmail.com>
2023-07-14 07:36:01 +0530

    Grocery Link

--- 09-Optimising our App/src/components/Header.js
+++ 09-Optimising our App/src/components/Header.js
@@ -1,9 +1,12 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
-  
+import useOnlineStatus from "../../utils/useOnlineStatus";
+
 const Header = () => {
   const [btnNameReact, setbtnNameReact] = useState("Login");
+
+  const onlineStatus = useOnlineStatus();
 
   return (
     <div className="header">
@@ -12,7 +15,10 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li> 
+          <li>
+          {onlineStatus ? '✅ Online' : '❌ Disconnected'}
+          </li>
+          <li>
             <Link to="/">Home</Link>
           </li>
           <li>
@@ -20,6 +26,9 @@ const Header = () => {
           </li>
           <li>
             <Link to="/contact">Contact Us</Link>
+          </li>
+          <li>
+            <Link to="/grocery">Grocery</Link>
           </li>
           <li>
             <Link to="">Cart</Link>
